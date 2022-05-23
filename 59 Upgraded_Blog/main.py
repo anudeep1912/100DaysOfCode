22e767e5411d4c14d9d1d289787a2922a8d742c5
Anudeep P <pregadaanudeep@gmail.com>
2022-05-23 23:50:27 +0530

    Added the contact page feature

--- 59 Upgraded_Blog/main.py
+++ 59 Upgraded_Blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from post import Post
 
@@ -26,9 +26,12 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact.html")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html", msg_sent=False)
+    elif request.method == "POST":
+        return render_template("contact.html", msg_sent=True)
 
 
 @app.route("/post/<int:index>")
