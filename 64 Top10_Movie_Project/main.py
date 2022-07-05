f15b9a915a7b9636d06a8bf9acbb4c0411059876
Anudeep P <pregadaanudeep@gmail.com>
2022-07-05 22:48:58 +0530

    Added 64th day project. my movie db

--- 64 Top10_Movie_Project/main.py
+++ 64 Top10_Movie_Project/main.py
@@ -8,6 +8,8 @@ import requests
 
 MOVIE_DB_API_KEY = "27cc31d21b7faa36b053eb3ca40c2ac2"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -26,23 +28,22 @@ class Movie(db.Model):
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(250), unique=True, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
     img_url = db.Column(db.String(500), nullable=False)
-
 
     # Optional: this will allow each book object to be identified by its title when printed.
     def __repr__(self):
         return f'<Book {self.title}>'
-db.create_all()
 
 
 class EditForm(FlaskForm):
     # A form Object inheriting from WTForms
     rating = StringField('Your Rating out of 10 E.g. 7.5', validators=[DataRequired()], render_kw={"style": "font-weight: bold;"})
-    review = StringField('Your Review.', validators=[DataRequired()],render_kw={"style": "font-weight: bold;"})
+    review = StringField('Your Review.', validators=[DataRequired()], render_kw={"style": "font-weight: bold;"})
     submit = SubmitField('Done')
+
 
 class AddForm(FlaskForm):
     # A form Object inheriting from WTForms
@@ -50,23 +51,12 @@ class AddForm(FlaskForm):
     submit = SubmitField('Add Movie')
 
 
-# # After adding the new_movie the code needs to be commented out/deleted.
-# # So you are not trying to add the same movie twice.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
-
 @app.route("/")
 def home():
-    all_movies = Movie.query.all()
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+        db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 
@@ -101,9 +91,27 @@ def add():
         response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
         data = response.json()["results"]
         return render_template("select.html", options=data)
+    return render_template("add.html", form=add_form)
 
-    return render_template("edit.html", form=add_form)
+
+@app.route("/find", methods=['GET', 'POST'])
+def find():
+    movie_id = request.args.get("id")
+    if movie_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('edit', id=new_movie.id))
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
